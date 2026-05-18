1d18a68b14532444c33d8768aeb2253ca2126f43
Avril Correia <avril.correia@eia.edu.co>
2026-05-18 15:27:57 -0500

    Agrego la clase LCD

--- Lab4/lib/LCD/LCD.cpp
+++ Lab4/lib/LCD/LCD.cpp
@@ -1,17 +1,22 @@
 #include "LCD.h"
 
-LCD::LCD(I2CMaster *i2c, uint8_t addr)
+LCD::LCD(i2c_port_t port, uint8_t addr)
 {
-    _i2c = i2c;
+    _port = port;
     _addr = addr;
 }
 
 void LCD::writeByte(uint8_t data)
 {
-    uint8_t buffer[1];
-    buffer[0] = data;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create(); //crea la transaccion
 
-    _i2c->writeRawBytes(_addr, buffer, 1);
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (_addr << 1) | I2C_MASTER_WRITE, true);
+    i2c_master_write_byte(cmd, data, true);
+    i2c_master_stop(cmd);
+
+    i2c_master_cmd_begin(_port, cmd, pdMS_TO_TICKS(1000));
+    i2c_cmd_link_delete(cmd);
 }
 
 void LCD::sendNibble(uint8_t nibble, uint8_t rs)
@@ -58,8 +63,42 @@ void LCD::init()
     sendNibble(0x20, 0);
     vTaskDelay(pdMS_TO_TICKS(5));
 
-    command(0x28);
-    command(0x0C);
-    command(0x06);
+    command(0x28); // 4 bits, 2 líneas, fuente 5x8
+    command(0x0C); // display ON, cursor OFF
+    command(0x06); // cursor incrementa hacia la derecha
     clear();
+}
+
+void LCD::clear()
+{
+    command(0x01);
+    vTaskDelay(pdMS_TO_TICKS(5));
+}
+
+void LCD::setCursor(uint8_t fila, uint8_t columna)
+{
+    if (fila == 0) {
+        command(0x80 + columna);
+    } else {
+        command(0xC0 + columna);
+    }
+}
+
+void LCD::print(const char *texto)
+{
+    while (*texto) {
+        data(*texto);
+        texto++;
+    }
+}
+
+void LCD::mostrarMensaje(const char *lineaSuperior, const char *lineaInferior)
+{
+    clear();
+
+    setCursor(0, 0);
+    print(lineaSuperior);
+
+    setCursor(1, 0);
+    print(lineaInferior);
 }
